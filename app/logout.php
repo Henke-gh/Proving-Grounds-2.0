91a398338dcc56b9491d801de1efc7df7ee76ec0
Henke-gh <77665143+Henke-gh@users.noreply.github.com>
2024-02-16 15:09:20 +0100

    small logout fix

--- app/logout.php
+++ app/logout.php
@@ -7,18 +7,20 @@ require __DIR__ . "/../bootstrap.php";
 use App\Hero;
 
 if (isset($_POST['logout'])) {
-    $_SESSION['player'] = $database->getHero($_SESSION['playerID']);
-    $playerSaveState = $_SESSION['player'];
-    $player = new Hero($playerSaveState['name'], $playerSaveState['gender']);
-    $player->loadHeroState($playerSaveState);
+    if (!empty($_SESSION['player'])) {
+        $_SESSION['player'] = $database->getHero($_SESSION['playerID']);
+        $playerSaveState = $_SESSION['player'];
+        $player = new Hero($playerSaveState['name'], $playerSaveState['gender']);
+        $player->loadHeroState($playerSaveState);
 
-    $player->regenerateHPnGrit();
-    $_SESSION['player'] = $player->saveHeroState();
-    $saveHero = serialize($_SESSION['player']);
+        $player->regenerateHPnGrit();
+        $_SESSION['player'] = $player->saveHeroState();
+        $saveHero = serialize($_SESSION['player']);
 
-    $database->updateHero($_SESSION['playerID'], $saveHero);
+        $database->updateHero($_SESSION['playerID'], $saveHero);
 
-    unset($_SESSION['player']);
+        unset($_SESSION['player']);
+    }
     unset($_SESSION['playerID']);
 
     header('Location: /../index.php');
