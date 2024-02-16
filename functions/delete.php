c970c1692319fe489e8d642e375442bb844e530b
Henke-gh <77665143+Henke-gh@users.noreply.github.com>
2024-02-16 15:01:36 +0100

    added temp hero delete btn

---
+++ functions/delete.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+session_start();
+require __DIR__ . "/../bootstrap.php";
+if (isset($_SESSION['playerID'])) {
+
+    if (isset($_POST['deleteHero'])) {
+        $id = $_SESSION['playerID'];
+        $database->deleteHero($id);
+        unset($_SESSION['player']);
+        header('Location: /../app/heroCreation_step1.php');
+        exit();
+    }
+} else {
+    header('Location: /../index.php');
+    exit();
+}
--- nav/footer.php
+++ nav/footer.php
@@ -3,6 +3,9 @@
         <form method="post" action="/../app/logout.php">
             <button type="submit" name="logout">Log Out</button>
         </form>
+        <form method="post" action="/../functions/delete.php">
+            <button type="submit" name="deleteHero">Delete Hero</button>
+        </form>
     <?php endif; ?>
 </footer>
 </body>
